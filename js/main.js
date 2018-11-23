bb97988836fa45b8757a37aeb4955c3af180cb12
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-11-23 19:42:35 +0300

    pwm&serial

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 let atmega = document.querySelector(".atmega"),
     analog = document.querySelector(".analog"),
     digital = document.querySelector(".digital"),
+    pwm = document.querySelector(".pwm"),
     modalAtmega = document.querySelector(".modal-atmega"),
     modalAnalog = document.querySelector(".modal-analog"),
     modalDigital = document.querySelector(".modal-digital"),
+    modalPwm = document.querySelector(".modal-pwm"),
     btnClose = document.querySelectorAll(".close-btn");
 
     modalAtmega.style.display = "none";
     modalAnalog.style.display = "none";
     modalDigital.style.display = "none";
+    modalPwm.style.display = "none";
 
     atmega.addEventListener('click', () => {
         modalAtmega.style.display = "block";
@@ -18,6 +21,9 @@ let atmega = document.querySelector(".atmega"),
     });
     digital.addEventListener('click', () => {
         modalDigital.style.display = "block";
+    });
+    pwm.addEventListener('click', () => {
+        modalPwm.style.display = "block";
     });
 
     btnClose.forEach((item) => {
