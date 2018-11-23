aa5c6530ec90f9802a7a1b99c9f8040612ad9c5f
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-11-23 12:54:19 +0300

    analog & digital

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 let atmega = document.querySelector(".atmega"),
+    analog = document.querySelector(".analog"),
+    digital = document.querySelector(".digital"),
     modalAtmega = document.querySelector(".modal-atmega"),
+    modalAnalog = document.querySelector(".modal-analog"),
+    modalDigital = document.querySelector(".modal-digital"),
     btnClose = document.querySelectorAll(".close-btn");
 
     modalAtmega.style.display = "none";
+    modalAnalog.style.display = "none";
+    modalDigital.style.display = "none";
 
     atmega.addEventListener('click', () => {
         modalAtmega.style.display = "block";
+    });
+    analog.addEventListener('click', () => {
+        modalAnalog.style.display = "block";
+    });
+    digital.addEventListener('click', () => {
+        modalDigital.style.display = "block";
     });
 
     btnClose.forEach((item) => {
